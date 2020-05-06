2ebcd6829f01ab93b1d720d43663c10df7a99824
Jakub Konefał <konefaljakub@gmail.com>
2020-05-06 12:59:08 +0200

    -  Finish landing page

--- src/components/Outside/Outside.js
+++ src/components/Outside/Outside.js
@@ -1,36 +1,41 @@
 import React from "react";
 import classes from "./Outside.module.css";
+import Logo from "../../assets/img/logo.png";
 
 const Outside = () => (
   <div className={classes.Background}>
-    <nav className={classes.Navigation}>
-      <h2 className={classes.Navigation__Logo}>team manager logo</h2>
-      <div className={classes.Navigation__Items}>
-        <span
-          className={`${classes.Navigation__Item} ${classes.Navigation__ItemText}`}
-        >
-          Home
-        </span>
-        <span
-          className={`${classes.Navigation__Item} ${classes.Navigation__ItemText}`}
-        >
-          Contact
-        </span>
-        <div className={classes.Navigation__Buttons}>
-          <button
-            className={`${classes.Navigation__Button_SignIn} ${classes.Navigation__Button}`}
-          >
-            sign up
-          </button>
-          <button
-            className={`${classes.Navigation__Button_Login} ${classes.Navigation__Button}`}
-          >
-            login
-          </button>
+    <div className={classes.Wraper}>
+      <nav className={classes.Navigation}>
+        <img src={Logo} className={classes.Navigation__Logo} alt="Logo" />
+        <h4 className={classes.Navigation__LogoLabel}>localCoach</h4>
+        <div className={classes.Navigation__Items}>
+          <span className={classes.Navigation__ItemText}>Home</span>
+          <span className={classes.Navigation__ItemText}>Contact</span>
+          <div className={classes.Navigation__Buttons}>
+            <button
+              className={`${classes.Navigation__Button_SignIn} ${classes.Navigation__Button}`}
+            >
+              sign up
+            </button>
+            <button
+              className={`${classes.Navigation__Button_Login} ${classes.Navigation__Button}`}
+            >
+              login
+            </button>
+          </div>
         </div>
+      </nav>
+      <div className={classes.Description}>
+        <h2 className={classes.Description__Football_Label}>football</h2>
+        <h3 className={classes.Description__TeamManager_Label}>
+          team - manager
+        </h3>
+        <p className={classes.Description__AppDescription}>
+          Manage your football teams online. Gather all information about teams,
+          players and trainings in one place!
+        </p>
       </div>
-    </nav>
-    <main></main>
+    </div>
   </div>
 );
 
